1587d86fe14d41835b78f43ea3398df2c0aeac4a
Robertgervais1 <Robertgervais22@gmail.com>
2018-11-27 15:59:37 -0600

    fix broken spec

--- spec/workers/ballot_check_worker_spec.rb
+++ spec/workers/ballot_check_worker_spec.rb
@@ -1,6 +1,6 @@
-require("rails_helper")
-class BallotCheckWorkerTest < Minitest::Test
-  it("example") do
+require "rails_helper"
+describe "BallotCheckWorker" do 
+  it "example" do
     skip("add some examples to (or delete) test/workers/ballot_check_worker_test.rb")
   end
 end
