321b65567c721e25a00b52edc06528909b309725
Bobby <Robertgervais22@gmail.com>
2019-06-28 20:38:01 -0500

    lol passing my tests

--- spec/factories/ballots.rb
+++ spec/factories/ballots.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :ballot do
     user { FactoryBot.create(:user) }
+    options { "Yes, No" }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "email#{n}@cccballot.com" }
+    password { "hellopassword" }
   end
 end
