caa96f48d6e1cf0ea5363d8d724912a28b5bcb3b
Bobby <Robertgervais22@gmail.com>
2019-08-29 15:33:22 -0500

    Revert "manually send reminder vote"
    This reverts commit 75cf9b52bcbaf804019a017ca8e9fa515dd78cfb.

--- app/controllers/ballots_controller.rb
+++ app/controllers/ballots_controller.rb
@@ -82,7 +82,7 @@ class BallotsController < ApplicationController
     @users = find_users_that_have_not_voted(params[:format])
     @ballot_id = params[:format]
     @ballot = Ballot.find(params[:format])
-    UsersNotifier.send_reminder(@ballot, @users).deliver
+    #need to add mailer here
     redirect_to ballots_path
   end
 
