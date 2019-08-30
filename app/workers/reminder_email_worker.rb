efb4c7c2fbc66b11bc85a54705b7bd099456bb80
Bobby <Robertgervais22@gmail.com>
2019-08-30 12:27:27 -0500

    minor change

--- app/workers/reminder_email_worker.rb
+++ app/workers/reminder_email_worker.rb
@@ -4,10 +4,12 @@ class ReminderEmailWorker
   def perform
     ReminderEmail.where("next_run_at <= ?", Time.now).find_each do |job|
       @ballot = Ballot.find(job.ballot_id)
-      unless @ballot.expired? && !@ballot.enough_votes?
+      if !@ballot.enough_votes?
         @users = @ballot.find_users_that_have_not_voted(@ballot.id)
         UsersNotifier.send_reminder(@ballot, @users).deliver
         job.update_attribute!(next_run_at: job.next_run_at + 1.day)
+      else
+        job.destroy
       end
     end
   end
