40213c988667057abb0a29358c13cbcc75df9c77
Bobby <rgervais@enova.com>
2023-02-02 00:14:48 -0500

    remove mailers for now

--- app/mailers/users_notifier.rb
+++ app/mailers/users_notifier.rb
@@ -1,34 +1,34 @@
 
-class UsersNotifier < ActionMailer::Base
-  default :from => "ccballot.sick&tight.com"
+# class UsersNotifier < ActionMailer::Base
+#   default :from => "ccballot.sick&tight.com"
 
-  def send_signup_email(user)
-    @user = user
-    mail(:to => @user.email,
-         :subject => "One of us!")
-  end
+#   def send_signup_email(user)
+#     @user = user
+#     mail(:to => @user.email,
+#          :subject => "One of us!")
+#   end
 
-  def ballot_email(ballot)
-    @url = "http://shielded-falls-33734.herokuapp.com/ballots/#{ballot.id}"
-    @issue = ballot.ballot_issue
-    @creator = ballot.user
-    @bcc = User.all.pluck(:email)
-    mail(:bcc => @bcc,
-         :subject => "New ballot, cats!")
-  end
+#   def ballot_email(ballot)
+#     @url = "http://shielded-falls-33734.herokuapp.com/ballots/#{ballot.id}"
+#     @issue = ballot.ballot_issue
+#     @creator = ballot.user
+#     @bcc = User.all.pluck(:email)
+#     mail(:bcc => @bcc,
+#          :subject => "New ballot, cats!")
+#   end
 
-  def close(ballot)
-    @issue = ballot.ballot_issue
-    @totals = ballot.tally
-    @bcc = User.all.pluck(:email)
-    mail(:bcc => @bcc,
-         :subject => "#{@issue}'s been closed!")
-  end
+#   def close(ballot)
+#     @issue = ballot.ballot_issue
+#     @totals = ballot.tally
+#     @bcc = User.all.pluck(:email)
+#     mail(:bcc => @bcc,
+#          :subject => "#{@issue}'s been closed!")
+#   end
 
-  def send_reminder(ballot, users)
-    @issue = ballot.ballot_issue
-    @users = users.pluck(:email)
-    mail(:bcc => @users,
-         :subject => "Helllooooo please vote on #{@issue}")
-  end
-end
+#   def send_reminder(ballot, users)
+#     @issue = ballot.ballot_issue
+#     @users = users.pluck(:email)
+#     mail(:bcc => @users,
+#          :subject => "Helllooooo please vote on #{@issue}")
+#   end
+# end
